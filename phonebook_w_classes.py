6a5e24f37a9c2a113c0f9e03abf8000f9f81f752
Kyle Booth <kbooth1000@gmail.com>
2018-02-06 21:55:27 -0500

    add data-save/retrieve functionality

--- phonebook_w_classes.py
+++ phonebook_w_classes.py
@@ -1,3 +1,5 @@
+import os, pickle
+
 phonebook = []
 
 class Person(object):
@@ -7,35 +9,58 @@ class Person(object):
         self.email = email
         self.phone = phone
 
+def save_data():
+    global phonebook
+    phonebook_data_file = open('phonebook_data.pickle', 'w+')
+    pickle.dump(phonebook, phonebook_data_file)
+    phonebook_data_file.close()
+
+def retrieve_data():
+    global phonebook
+    if os.path.isfile("phonebook_data.pickle") == True:
+        retrieved_data_file = open('phonebook_data.pickle', 'r+')
+        phonebook = pickle.load(retrieved_data_file)
+        retrieved_data_file.close()
+        print '**** RETRIEVED ****\n'
+        print phonebook[0].first
+    else:
+        phonebook = []
+
 user_action = 0
-while user_action != 5:
+while user_action != 7:
 
     print '''
     1. Look up an entry
     2. Set an entry
     3. Delete an entry
     4. List all entries
-    5. Quit
+    5. Save all entries
+    6. Retrieve saved entries
+    7. Quit
     
     '''
     while True:
         try:
-            user_action = int(raw_input('What do you want to do (1-5)? '))
+            user_action = int(raw_input('What do you want to do (1-7)? '))
         except ValueError:
             user_action = 0
-            print '\n*----Enter a number (1-5), please.\n'
+            print '\n*----Enter a number (1-7), please.\n'
         break
 # LOOK UP ENTRY
     if user_action == 1:
+        person_found = False
         person_to_find = raw_input('Person to Find (first name): ')
-        for i in range(0, len(phonebook)):
-            if phonebook[i].first == person_to_find:
-                print '*******\n%s %s, %s, %s' % (
-                    phonebook[i].first, 
-                    phonebook[i].last, 
-                    phonebook[i].email, 
-                    phonebook[i].phone
+        for entry in phonebook:
+            if entry.first == person_to_find:
+                person_to_find = True
+                print '\n*** FOUND ***\n %s %s, %s, %s' % (
+                    entry.first, 
+                    entry.last, 
+                    entry.email, 
+                    entry.phone
                 ) 
+        if person_found == False:
+            print '\n** NOT FOUND **\n'
 
 
 # SET ENTRY
@@ -51,8 +76,8 @@ while user_action != 5:
     if user_action == 3:
         person_to_remove = raw_input('Person to Remove (first name): ')
         for i in range(0, len(phonebook)):
-            if phonebook[i].first == person_to_find:
-                are_you_sure = raw_input('Remove %s %s? ' % (phonebook[i].first, phonebook[i].last))
+            if phonebook[i].first == person_to_remove:
+                are_you_sure = raw_input('Remove %s %s (Y/N)? ' % (phonebook[i].first, phonebook[i].last))
                 if are_you_sure.upper() == 'Y' or are_you_sure.upper() == 'YES':
                     phonebook.remove(phonebook[i])
                 else:
@@ -64,6 +89,16 @@ while user_action != 5:
             print '\n***%s %s, %s, %s' % (
                 phonebook[i].first, phonebook[i].last, phonebook[i].email, phonebook[i].phone
                 )
+
+# SAVE ENTRIES
+    if user_action == 5:
+        save_data()
+        print '\n***********\n** Saved **\n***********\n'
+
+# RETRIEVE ENTRIES
+    if user_action == 6:
+        retrieve_data()
+
 
 
 print 'Bye!'  
